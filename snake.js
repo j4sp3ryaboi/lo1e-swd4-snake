d35a1b50b0b7fb9af014ecdc304a1b0832d9c286
j4sp3ryaboi <95280432+j4sp3ryaboi@users.noreply.github.com>
2022-09-13 14:14:15 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,5 +1,50 @@
-const canvas = document.getElementById("snake");
-const ctx = canvas.getContext("2d");
+let canvas = document.getElementById("snake");
+let ctx = canvas.getContext("2d");
 
+let snakeX = 200;
+let snakeY = 200;
+let direction = '';
+
+function drawBackground(){
 ctx.fillStyle = "black";
-ctx.fillRect(10, 10, 150, 100);
+ctx.fillRect(0, 0, 400, 400);
+}
+function drawSnake(){
+ctx.fillStyle = "blue";
+ctx.fillRect(snakeX, snakeY, 20, 20);
+}
+
+function update() {
+    if(direction == 'right'){
+    snakeX += 20;
+    } else if(direction == 'left'){
+        snakeX -= 20;
+    }else if(direction == 'down'){
+        snakeY += 20;
+    }else if(direction == 'up'){
+        snakeY -= 20;
+    }
+
+    drawBackground();
+    drawSnake();
+}
+
+function changeDirection(event){
+    if(event.code == 'ArrowUp'){
+        direction = 'up';
+    }if(event.code == 'ArrowDown'){
+        direction = 'down';
+    }if(event.code == 'ArrowRight'){
+        direction = 'right';
+    }if(event.code == 'ArrowLeft'){
+        direction = 'left';
+    }
+
+}
+
+
+drawBackground();
+drawSnake();
+
+setInterval(update, 25)
+addEventListener('keydown', changeDirection);
